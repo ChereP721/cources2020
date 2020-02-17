5d79109376f42a232d97145bc03c8fce379a3631
a.norin <norin.alexey@tradesoft.ru>
2020-02-17 21:24:50 +0300

    Корректировка по замечаниям

--- PHP 1 lesson/user1/sum_less.php
+++ PHP 1 lesson/user1/sum_less.php
@@ -1,15 +1,17 @@
 <?php
-function SumLess ($numer){
-    if(is_numeric($numer) and is_int($numer)){
-        $sum = 0;
-        while ($numer>0){
-            $numer--;
-            $sum += $numer;
+declare(strict_types=1);
 
-        }
-
+/**
+ * @param int $number
+ * @return int
+ */
+function sumLess(int $number): int
+{
+    $sum = 0;
+    while ($number > 0) {
+        $number--;
+        $sum += $number;
     }
-    else echo 'Введите целое цчисло';
 
     return $sum;
 }
