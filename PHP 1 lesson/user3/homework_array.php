347cbca42e51a01941051fd8470c7c5495bda2a9
cherep721 <alexnorin721@gmail.com>
2020-03-12 23:18:48 +0300

    Revert "user3 check"
    This reverts commit 9a285891

--- PHP 1 lesson/user3/homework_array.php
+++ PHP 1 lesson/user3/homework_array.php
@@ -1,23 +1,22 @@
 <?php
-
 //Найти медиану предыдущего массива и отбросить все  элементы, которые больше и меньше медианы в 2 раза
-$array = [];
+$array = array ();
 
-$n = 20;
-for ($i = 0; $i < $n; $i++) {
-    $array[] = rand(1, 50);
-    echo $array[$i] . "\t";
+$n=20;
+for($i=0;$i<$n;$i++){
+	$array[]= rand(1,50);
+	echo $array[$i]."\t";
 }
-
-$med = $n / 2;
-$n1 = $n;
-for ($i = 0; $i < $n; $i++) {
-    if (!$array[$med + 1] && $array[$i] / 2 == $array[$med + 1] || $array[$i] * 2 == $array[$med + 1]) {
-        unset($array[$i]);
-        $n1 = $n - 1;
-    }
+$med=$n/2;
+$n1=$n;
+for($i=0;$i<$n;$i++){
+	if(!$array[$med+1]&&$array[$i]/2==$array[$med+1]||$array[$i]*2==$array[$med+1]){
+		unset($array[$i]);
+		$n1=$n-1;
+	}
 }
 echo "<br/>";
-for ($i = 0; $i < $n1; $i++) {
-    echo $array[$i] . "\t";
+for($i=0;$i<$n1;$i++){
+	echo $array[$i]."\t";
 }
+?>
